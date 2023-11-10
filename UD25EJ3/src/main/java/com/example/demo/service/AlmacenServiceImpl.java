6c548b51125791991f10a83b266639f91830d5d8
carlavelasco15 <carlavelasco15@gmail.com>
2023-11-10 13:58:23 +0100

    add missing annotations

--- UD25EJ3/src/main/java/com/example/demo/service/AlmacenServiceImpl.java
+++ UD25EJ3/src/main/java/com/example/demo/service/AlmacenServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.dao.IAlmacenDAO;
 import com.example.demo.dto.Almacen;
 
+@Service
 public class AlmacenServiceImpl {
 
 	@Autowired
--- UD25EJ3/src/main/java/com/example/demo/service/CajaServiceImpl.java
+++ UD25EJ3/src/main/java/com/example/demo/service/CajaServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.dao.ICajaDAO;
 import com.example.demo.dto.Caja;
 
+@Service
 public class CajaServiceImpl {
 
 	@Autowired
