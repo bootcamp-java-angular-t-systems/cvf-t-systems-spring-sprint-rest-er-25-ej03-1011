683f14e6ee3905b572a39fde12a71c6f7cd1d9e9
carlavelasco15 <carlavelasco15@gmail.com>
2023-11-11 00:20:47 +0100

    5 sql inserts per table

---
+++ UD25EJ3/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+-- Inserts para la tabla "almacen"
+INSERT INTO almacen (lugar, capacidad) VALUES ('Almacen1', 100);
+INSERT INTO almacen (lugar, capacidad) VALUES ('Almacen2', 150);
+INSERT INTO almacen (lugar, capacidad) VALUES ('Almacen3', 200);
+INSERT INTO almacen (lugar, capacidad) VALUES ('Almacen4', 120);
+INSERT INTO almacen (lugar, capacidad) VALUES ('Almacen5', 180);
+
+-- Inserts para la tabla "cajas"
+INSERT INTO cajas (num_referencia, contenido, valor, almacen_id) VALUES ('A123', 'Objetos1', 50, 1);
+INSERT INTO cajas (num_referencia, contenido, valor, almacen_id) VALUES ('B456', 'Objetos2', 75, 2);
+INSERT INTO cajas (num_referencia, contenido, valor, almacen_id) VALUES ('C789', 'Objetos3', 100, 3);
+INSERT INTO cajas (num_referencia, contenido, valor, almacen_id) VALUES ('D101', 'Objetos4', 30, 4);
+INSERT INTO cajas (num_referencia, contenido, valor, almacen_id) VALUES ('E202', 'Objetos5', 90, 5);
