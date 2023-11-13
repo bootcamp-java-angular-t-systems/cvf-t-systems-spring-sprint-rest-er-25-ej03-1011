32772d2f4bc80afaef8829c667d46afa083f0a22
carlavelasco15 <carlavelasco15@gmail.com>
2023-11-13 02:49:01 +0100

    refix request mapping from almacencontroller

--- UD25EJ3/src/main/java/com/example/demo/controller/CajaController.java
+++ UD25EJ3/src/main/java/com/example/demo/controller/CajaController.java
@@ -16,7 +16,7 @@ import com.example.demo.dto.Caja;
 import com.example.demo.service.CajaServiceImpl;
 
 @RestController
-@RequestMapping(name = "/caja")
+@RequestMapping("/caja")
 public class CajaController {
 
 	@Autowired
